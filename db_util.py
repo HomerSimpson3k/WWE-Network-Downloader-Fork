fec66a541521f423eba167a4bcf3ff252c7f555b
h0mer <marcel@gressler.net>
2024-12-24 12:29:18 +0100

    Fix problem with video_name on db operations

--- db_util.py
+++ db_util.py
@@ -9,6 +9,8 @@ class database:
         self.conn = sqlite3.connect(CONSTANTS.DB_FILE)
         self.c = self.conn.cursor()
 
+    def clean_string(self, text):
+        return text.replace("'", "")
 
     # Connect to the database. If the file doesn't exist, it creates it.
     def db_connect(self):
@@ -29,14 +31,14 @@ class database:
     # Insert download into the database
     def db_ins(self, video_id, video_name, video_qual, partial, timestamp):
         try:
-            self.c.execute(f"INSERT INTO downloads VALUES ('{video_id}', '{video_name}', '{video_qual}',  '{partial}', '{timestamp}')")
+            self.c.execute(f"INSERT INTO downloads VALUES ('{video_id}', '{self.clean_string(video_name)}', '{video_qual}',  '{partial}', '{timestamp}')")
             self.conn.commit()
         except sqlite3.IntegrityError: 
             print(f"Error: Couldn't add {video_id} to the database. ID already exists.")
 
     # Update download information in the database
     def db_upd(self, video_id, video_name, video_qual, partial, timestamp):
-        self.c.execute(f"UPDATE downloads SET name = '{video_name}', quality = '{video_qual}', partial_download = '{partial}', date = '{timestamp}' WHERE id = {video_id}")
+        self.c.execute(f"UPDATE downloads SET name = '{self.clean_string(video_name)}', quality = '{video_qual}', partial_download = '{partial}', date = '{timestamp}' WHERE id = {video_id}")
         self.conn.commit()
 
     # Query the database for previously downloaded episode
